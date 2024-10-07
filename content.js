cbc1523101b812143cc5c21573951a1ec754d8b8
Auric Vente <mprops@protonmail.com>
2024-10-07 06:41:56 -0600

    first commit

---
+++ content.js
@@ -0,0 +1,15 @@
+let anchors = document.getElementsByTagName(`a`)
+
+for (let anchor of anchors) {
+  anchor.setAttribute(`target`, `_blank`)
+  anchor.classList.add(`action_link`)
+}
+
+let images = document.getElementsByTagName(`img`)
+let icon_path = chrome.runtime.getURL(`icon.jpg`)
+
+for (let img of images) {
+  if (img.src.endsWith(`y18.svg`)) {
+    img.src = icon_path
+  }
+}
